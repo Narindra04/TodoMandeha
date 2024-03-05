44d800985c0ab5a3ff6557024401ff048cc2a975
Faniloniaina ANDRIAMANANA <a.faniloniaina@gmail.com>
2024-03-05 13:28:47 +0300

    auth

--- TodoMandeha/Models/DBConnexion.cs
+++ TodoMandeha/Models/DBConnexion.cs
@@ -7,9 +7,28 @@ using System.Web;
 
 namespace TodoMandeha.Models
 {
-    public static class DBConnexion
+    public class DBConnexion
     {
         public static NpgsqlConnection connectionString =
             new NpgsqlConnection(ConfigurationManager.ConnectionStrings["todolist"].ConnectionString);
+
+        public static bool VerifierUtilisateur(Utilisateur utilisateur)
+        {
+            var request = "SELECT * FROM public.\"User\" WHERE username='" + utilisateur.Username + "' AND password='" + utilisateur.Password + "'";
+            var hasUser = false;
+            try
+            {
+                connectionString.Open();
+                var cmd = new NpgsqlCommand(request, connectionString);
+                var reader = cmd.ExecuteReader();
+                hasUser = reader.HasRows;
+                connectionString.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return hasUser;
+        }
     }
 }
---
+++ TodoMandeha/Models/Utilisateur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoMandeha.Models
+{
+    public class Utilisateur
+    {
+        private string _username;
+        private string _password;
+        public string Password { get => _password; set => _password = value; }
+        public string Username { get => _username; set => _username = value; }
+
+        public Utilisateur() { }
+        public Utilisateur(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+    }
+}
