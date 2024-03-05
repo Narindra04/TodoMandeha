22016a237b851a04f722697ab4fc3e717c52645a
Faniloniaina ANDRIAMANANA <a.faniloniaina@gmail.com>
2024-03-05 14:15:43 +0300

    oups

--- TodoMandeha/Models/Tache.cs
+++ TodoMandeha/Models/Tache.cs
@@ -7,5 +7,49 @@ namespace TodoMandeha.Models
 {
     public class Tache
     {
+
+        private int _tacheid;
+        private string _username;
+        private string _description;
+        private bool _state;
+
+        public Tache() { }
+
+        public Tache(int tacheid, string username, string description, bool state)
+        {
+            _tacheid = tacheid;
+            _username = username;
+            _description = description;
+            _state = state;
+        }
+
+        public int Tacheid
+        {
+            get { return _tacheid; }
+            set { _tacheid = value; }
+        }
+
+
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+
+
+        public bool State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
     }
 }
